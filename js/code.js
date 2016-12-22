ac480f5fd91150ba67f236245f13f56e84733d54
qinzengfu <15938365700@163.com>
2016-12-22 14:52:06 +0800

    视频跳转code

--- js/code.js
+++ js/code.js
@@ -2,22 +2,73 @@
 
 ;(function($){
   /*touchstart the title and drag the con moving*/
-  var moveX,moveY,startX,startY;
-  $(document).on("touchstart","#title",function(event){
-    if($(event.target).attr('id') == 'title'){
-      console.log(event.target.parentNode.offsetLeft)
+  var moveX,moveY,startX,startY,i=0;
+
+  var video=document.getElementsByTagName('video')
+  var len=$("video").length;
+  var v_width=parseFloat($("video").eq(0).css("width"));
+  $("#con").css("width",v_width*len)
+  $(document).on("touchstart","#con",function(event){
+    if($(event.target).hasClass('active')){
+      //console.log(event.target.parentNode.offsetLeft);
       var touchPros = event.touches[0];
       
       startX = touchPros.pageX - event.target.parentNode.offsetLeft;
       startY = touchPros.pageY - event.target.parentNode.offsetTop;
     }
     return false;
-  }).on("touchmove","#title",function(event){
-    if($(event.target).attr('id') == 'title'){
+  }).on("touchmove","#con",function(event){
+
+    if($(event.target).hasClass('active')){
+
+      console.log($("#con").css("width"));
+      console.log($("#con").css("height"));
+
+
       var touchPros = event.touches[0];
       moveX = touchPros.pageX - startX;
       moveY = touchPros.pageY - startY;
-      $('#con').css('left',moveX).css('top',moveY);
+      console.log(moveX)
+
+
+      $('#con').css('left',moveX);
       }
+
+  }).on("touchend","video",function(event){
+
+
+    console.log($(this))
+    var _this=$(this);
+    
+    var index=_this.index()
+    console.log(video[0])
+    console.log(video[1])
+    if(moveX<0){
+       
+
+       video[index].pause();
+       _this.removeClass("active");
+       
+       if(index==len-1){
+          index=0;
+       }else{
+         index=index+1;
+       }
+       $("video").eq(index).addClass("active");
+       video[index].play();
+       $('#con').css('left',-index*v_width+"px");
+    }else if(moveX>0){
+      
+       video[index].pause();
+       _this.removeClass("active");
+       if(index==0){
+        index=len-1;
+       }else{
+        index=index-1;
+       }
+       $("video").eq(index).addClass("active");
+       video[index].play();
+       $('#con').css('left',-index*v_width+"px");
+    }
   });
-})(Zepto); 
+})(Zepto); {}
